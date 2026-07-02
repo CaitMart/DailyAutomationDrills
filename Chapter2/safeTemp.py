bbf045afc9dc9b8cb587c58720fa60cc73fbb362
caitmart <martinezcaitlin13@gmail.com>
2026-07-01 19:32:25 -0700

    completed practice exercise

--- Chapter2/safeTemp.py
+++ Chapter2/safeTemp.py
@@ -1,25 +1,24 @@
-#rewrite code given to fix errors and ensure there is input valildation to decide if a given temperature is safe
+#rewrite code given to fix errors to decide if a given temperature is safe
 
 def userInput(): 
     scale = str(input("Enter C or F to indicate Celsius or Fahrenheit: "))
+    
     degrees = int(input("Enter the number of degrees: "))
     isSafe(scale, degrees)
 
-def scaleInputCheck(scale):
-    print(scale.lower())
-    if (scale.lower() != "c" or scale.lower() != "f"):
-        print("The scale you entered is not valid please try again")
-        userInput() 
-    return
 
 def isSafe(scale, degrees):
     scale = scale.lower()
     if scale == "c":
         if degrees >= 16 and degrees <= 38:
             print("celcius is safe")
+        else:
+            print("dangerous")
     elif scale == "f":
         if degrees >= 60.8 and degrees <= 100.4:
             print("Fahrenheit is safe")
+        else:
+            print("dangerous")
     else:
         print("Dangerous")
 
