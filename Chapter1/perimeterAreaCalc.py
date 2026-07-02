6cfd83eb8f087621a8f137ca1bfe3adec9905539
caitmart <martinezcaitlin13@gmail.com>
2026-07-01 18:51:17 -0700

    initial file and two main functions commit

---
+++ Chapter1/perimeterAreaCalc.py
@@ -0,0 +1,9 @@
+#write a program that accepts the length and width of a rectangle from the user and then calculate both the perimeter and area of the space. 
+
+#calculates the perimeter of the rectangle 
+def perimeter(length, width):
+    return 2 * (length + width)
+#calculates the area of the rectangle
+def area(length, width):
+    return length * width
+
