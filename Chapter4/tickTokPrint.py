5fd5b333571e50f14ec94f5bcd3c73e2a8b09993
Caitlin Martinez <37851864+CaitMart@users.noreply.github.com>
2026-07-07 13:03:11 -0700

    completed exercise
    script will print tic or tok every second that the user requested. Once it hits the number of seconds the program ends

--- Chapter4/tickTokPrint.py
+++ Chapter4/tickTokPrint.py
@@ -4,22 +4,17 @@ def userInput():
     secondsPaused = int(input("How many seconds for pause?: "))
     ticktok(secondsPaused)
 
-
 def ticktok(secondsPaused):
-    tick = True 
-
-    try:
-        while True:
-            if tick:
-                print("tick ", flush=True)
-            else: print("toc ", flush=True)
-            tick = not tick
-            time.sleep(secondsPaused-1)
-    
-    except: 
-        if(secondsPaused == 0): exit
-
-
+    tick = True
+    while secondsPaused != 0:
+        if tick:
+            print("tick ", flush=True)
+        else: 
+            print("toc ", flush=True)
+        tick = not tick
+        time.sleep(secondsPaused)
+        secondsPaused = secondsPaused - 1
+   
 def main(): 
     userInput()
 
